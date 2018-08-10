680edf2d889ba2dbc09919de1c58c0f0fb179fa5
MantehApps <mantas.simanauskas99@gmail.com>
2018-08-10 23:17:01 +0200

    v1.02

--- commands/ow.js
+++ commands/ow.js
@@ -6,14 +6,14 @@ const region = 'eu';
 
 module.exports.run = async (client, message, args) => {
 
-let platform;
-let name;
+let platform = "pc";
+let name = "Manteh-2615";
 
 if(!['pc','xbl','psn'].includes(args[0])) return message.channel.send('**Prašome rašyti komandą pagal formą: !fort [ pc | xbl | psn ] <vardas-1234>**');
 if (!args[1]) return message.channel.send('**Prašome rašyti komandą pagal formą: !fort [ pc | xbl | psn ] <vardas-1234>**');
 
-platform = args.shift();
-name = args.join(' ');
+//platform = args.shift();
+//name = args.join(' ');
 
     overwatch.getProfile(platform, region, name, (err, json) => {
 
